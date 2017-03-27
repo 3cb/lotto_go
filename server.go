bfe0a0469f4269ff818dc1cfc6c8fa9832be91c2
Marc CB <marc.cbrooks@gmail.com>
2017-03-27 17:40:18 -0400

    initialize vuejs/go project

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", HomeHandler)
+	n := negroni.New(negroni.NewStatic(http.Dir("static")))
+	n.UseHandler(r)
+	log.Fatal(http.ListenAndServe(":3000", n))
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	io.WriteString(w, "Welcome to the Homepage!!")
+}
