b5ade94439b1958dc1acdedc5f5e68bd419a5e03
Marc CB <marc.cbrooks@gmail.com>
2017-04-02 17:29:08 -0400

    add to gitignore

--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -12,13 +12,33 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", HomeHandler)
+	// r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/api/megamillions", MegaHandler)
+	r.HandleFunc("/api/powerball", PowerballHandler)
+
 	n := negroni.New(negroni.NewStatic(http.Dir("./")))
 	n.UseHandler(r)
 	log.Fatal(http.ListenAndServe(":3000", n))
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	io.WriteString(w, "Welcome to the Homepage!!")
+// func HomeHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.WriteHeader(200)
+// 	io.WriteString(w, "Welcome to the Homepage!!")
+// }
+
+func MegaHandler(w http.ResponseWriter, req *http.Request) {
+	dateQuery := []string{}
+	if err := json.NewDecoder(req.Body).Decode(&dateQuery); err != nil {
+		log.Println(err)
+	}
+
+}
+
+func PowerballHandler(w http.ResponseWriter, req *http.Request) {
+
+}
+
+type DateQuery struct {
+	Start string
+	End   string
 }
