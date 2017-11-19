1858b9ef2bc0a69c7c491634f3572e053edb86f0
Marc C-B <marc.cbrooks@gmail.com>
2017-11-19 14:48:21 -0500

    change server port

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:5000',
         secure: false
       }
     },
